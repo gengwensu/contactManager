9c1476307f72d7230f8f25a2be672fc9425d5e97
Su, Gary <gary.su@mastercard.com>
2019-04-24 14:31:18 -0400

    initial with all files

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
-import 'core-js/stable';
-import {Aurelia} from 'aurelia-framework'
-import environment from './environment';
-import {PLATFORM} from 'aurelia-pal';
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+import "core-js/stable";
+import { Aurelia } from "aurelia-framework";
+import environment from "./environment";
+import { PLATFORM } from "aurelia-pal";
 
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
-    .feature(PLATFORM.moduleName('resources/index'));
+    .feature(PLATFORM.moduleName("resources/index"));
 
-  aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
+  aurelia.use.developmentLogging(environment.debug ? "debug" : "warn");
 
   if (environment.testing) {
-    aurelia.use.plugin(PLATFORM.moduleName('aurelia-testing'));
+    aurelia.use.plugin(PLATFORM.moduleName("aurelia-testing"));
   }
 
-  aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
+  aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName("app")));
 }
